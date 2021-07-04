f20e662c3a5681eb04c856f292d733159a0acd4c
Antti Veikkolainen <antti.veikkolainen00@outlook.com>
2021-07-04 22:34:14 +0300

    Updated code

---
+++ index.js
@@ -0,0 +1,35 @@
+function GuardCaluse() {
+    return "";
+}
+
+function ConditionalStatements() {
+
+}
+
+
+function RemoveElse() {
+
+}
+
+function MultipleSameVales() {
+
+}
+
+function OneLineStatements() {
+
+}
+
+function Optimize() {
+    var code = document.getElementById("code").value;
+
+    // TODO:
+    // Check what needs to be optimized
+    // Handle optimization
+
+
+    return code;
+}
+
+function DetectOptimizationType() {
+
+}
